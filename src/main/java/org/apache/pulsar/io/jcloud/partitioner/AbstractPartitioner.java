bb35aa1b57b6067e758b2fe3d0f43feb2f70ee76
mukesh-ctds <ext-mukesh.kumar@datastax.com>
2025-04-17 15:24:30 +0530

    Added fix 4.0

--- src/main/java/org/apache/pulsar/io/jcloud/partitioner/AbstractPartitioner.java
+++ src/main/java/org/apache/pulsar/io/jcloud/partitioner/AbstractPartitioner.java
@@ -19,7 +19,9 @@
 package org.apache.pulsar.io.jcloud.partitioner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.pulsar.client.api.Message;
@@ -44,21 +46,45 @@ public abstract class AbstractPartitioner<T> implements Partitioner<T> {
     private boolean partitionerWithTopicName;
     private boolean useIndexAsOffset;
 
+    private Map<String, String> topicsToPathMapping;
+    private Map<String, String> computedTopicsToPathMapping;
+
     @Override
     public void configure(BlobStoreAbstractConfig config) {
         this.sliceTopicPartitionPath = config.isSliceTopicPartitionPath();
         this.withTopicPartitionNumber = config.isWithTopicPartitionNumber();
         this.useIndexAsOffset = config.isPartitionerUseIndexAsOffset();
         this.partitionerWithTopicName = config.isPartitionerWithTopicName();
+        this.topicsToPathMapping = parseTopicsToPathMappingString(config.getTopicsToPathMapping());
+        this.computedTopicsToPathMapping = new HashMap<>();
+    }
+
+    private Map<String, String> parseTopicsToPathMappingString(String topicsToPathMappingString) {
+        Map<String, String> topicsToPathMapping = new HashMap<>();
+        if (StringUtils.isNotEmpty(topicsToPathMappingString)) {
+            for (String topicPathPair : topicsToPathMappingString.split(",")) {
+                String[] topicToPathMapping = topicPathPair.split("=");
+                if (topicToPathMapping.length == 2) {
+                    topicsToPathMapping.put(topicToPathMapping[0], topicToPathMapping[1]);
+                } else {
+                    LOGGER.warn("The topic to path mapping is not in correct format {}.", topicPathPair);
+                }
+            }
+        }
+        LOGGER.info("The final topics to path mapping is: {}", topicsToPathMapping);
+        return topicsToPathMapping;
     }
 
     @Override
     public String generatePartitionedPath(String topic, String encodedPartition) {
-        List<String> joinList = new ArrayList<>();
-        if (partitionerWithTopicName) {
+        if (computedTopicsToPathMapping.containsKey(topic)) {
+            return String.format("%s%s%s", computedTopicsToPathMapping.get(topic), PATH_SEPARATOR, encodedPartition);
+        } else {
+            List<String> joinList = new ArrayList<>();
             TopicName topicName = TopicName.get(topic);
             joinList.add(topicName.getTenant());
             joinList.add(topicName.getNamespacePortion());
+
             if (topicName.isPartitioned() && withTopicPartitionNumber) {
                 if (sliceTopicPartitionPath) {
                     TopicName newTopicName = TopicName.get(topicName.getPartitionedTopicName());
@@ -71,9 +97,15 @@ public abstract class AbstractPartitioner<T> implements Partitioner<T> {
                 TopicName newTopicName = TopicName.get(topicName.getPartitionedTopicName());
                 joinList.add(newTopicName.getLocalName());
             }
+            String generatedTopicPath = StringUtils.join(joinList, PATH_SEPARATOR);
+            if (topicsToPathMapping.containsKey(generatedTopicPath)) {
+                joinList.clear();
+                joinList.add(topicsToPathMapping.get(generatedTopicPath));
+            }
+            computedTopicsToPathMapping.put(topic, StringUtils.join(joinList, PATH_SEPARATOR));
+            joinList.add(encodedPartition);
+            return StringUtils.join(joinList, PATH_SEPARATOR);
         }
-        joinList.add(encodedPartition);
-        return StringUtils.join(joinList, PATH_SEPARATOR);
     }
 
     protected String getFileName(Record<T> record) {
